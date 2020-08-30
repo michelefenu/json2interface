c92987011e58d8868216a1eebc4b6fae1350a266
Michele Fenu <michelefenu@gmail.com>
2020-08-30 17:32:24 +0200

    Generate interface for one-level JSON

--- src/json2interface.js
+++ src/json2interface.js
@@ -1,5 +1,25 @@
-export class json2interface {
-  hello () {
-    console.log('hello')
+export function generate (jsonData, rootNodeName = 'RootObject') {
+  let jsonObject = JSON.parse(jsonData)
+
+  if (Array.isArray(jsonObject) && jsonObject.length) {
+    jsonObject = jsonObject[0]
   }
+
+  const outputInterface = `interface ${rootNodeName} {\n`
+    .concat(
+      Object.keys(jsonObject)
+        .map(key => `  ${key}: ${_mapToDataType(key, jsonObject[key])};\n`)
+        .join('')
+    )
+    .concat(`}\n`)
+
+  return outputInterface
 }
+
+function _mapToDataType (key, value) {
+  const dataType = typeof value
+  if (dataType !== 'object') return dataType
+  else return key.charAt(0).toUpperCase() + key.slice(1)
+}
+
+export default generate
